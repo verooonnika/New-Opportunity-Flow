db44be73f4f548c8bea0c79abcda03eb7c12f886
verooonnika <veronikamail.21@gmail.com>
2020-11-04 23:20:37 +0300

    feat: add logic for switching to next screen after search

--- force-app/main/default/aura/accountSearch/accountSearchController.js
+++ force-app/main/default/aura/accountSearch/accountSearchController.js
@@ -1,21 +1,26 @@
 ({
-    onInit: function( component, event, helper ) {
-        var searchTerm = component.get( "v.searchTerm" );
-        helper.handleSearch( component, searchTerm );
+    onInit: function(component, event, helper){
+        helper.getAllAccounts(component, event, helper);
     },
     
-    onSearchTermChange: function( component, event, helper ) {
-        var searchTerm = component.get( "v.searchTerm" );
+    onSearchTermChange: function(component, event, helper){
+        var searchTerm = component.get("v.searchTerm");
         var delayMillis = 500;
-        var timeoutId = component.get( "v.searchTimeoutId" );
-        clearTimeout( timeoutId );
-        timeoutId = setTimeout( $A.getCallback( function() {
-            helper.handleSearch( component, searchTerm );
-        }), delayMillis );
-        component.set( "v.searchTimeoutId", timeoutId );
+        var timeoutId = component.get("v.searchTimeoutId");
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout( $A.getCallback( function(){
+            helper.handleSearch(component, searchTerm);
+        }), delayMillis);
+        component.set("v.searchTimeoutId", timeoutId);
     },
     
+    handleNewAccount: function(component, event, helper){
+        component.set("v.isNewAccount", true);
+        helper.nextFlowScreen(component, event, helper);
+    },
     
-    onRowAction: function( component, event, helper ) {
+    handleChooseAccount: function(component, event, helper){
+        component.set("v.isNewAccount", false);
+        helper.nextFlowScreen(component,event, helper);
     }
 })
--- force-app/main/default/aura/accountSearch/accountSearchHelper.js
+++ force-app/main/default/aura/accountSearch/accountSearchHelper.js
@@ -1,17 +1,26 @@
 ({
-    handleSearch: function( component, searchTerm ) {
-        var action = component.get( "c.searchAccounts" );
+    getAllAccounts: function(component, event, helper){
+        var action = component.get("c.getAllAccounts");
+        action.setCallback(this, function(response) {
+            this.accountsLoaded(component);
+            component.set("v.rows", response.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    },
+    
+    handleSearch: function(component, searchTerm){
+        var action = component.get("c.searchAccounts");
         action.setParams({
             searchTerm: searchTerm
         });
-        action.setCallback( this, function( response ) {
+        action.setCallback(this, function(response){
             this.accountsLoaded(component);
-            component.set( 'v.rows', response.getReturnValue() );
+            component.set("v.rows", response.getReturnValue());
         });
-        $A.enqueueAction( action );
+        $A.enqueueAction(action);
     },
     
-    accountsLoaded: function( component) {
+    accountsLoaded: function(component){
         var cols = [
             {
                 'label': 'Name',
@@ -27,8 +36,22 @@
                 'label': 'Website',
                 'fieldName': 'Website',
                 'type': 'url'
+            },
+            {
+                'label': 'Action',
+                'type': 'button',
+                'typeAttributes': {
+                    'label': 'Choose',
+                    'name': 'choose_account'
+                }
             }
         ];
-        component.set( 'v.cols', cols );
+        component.set("v.cols", cols);
+    }, 
+    
+    nextFlowScreen: function(component, event, helper){
+        var actionClicked = event.getSource().getLocalId();
+        var navigate = component.get("v.navigateFlow");
+        navigate(actionClicked);
     }
 })
